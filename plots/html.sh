2b0e2199eb7eba4ea8fc05e2d6002e0122bd65a4
Jennet Dickinson <dickinson.jennet@gmail.com>
2023-08-08 18:39:15 -0500

    add html scripts

---
+++ plots/html.sh
@@ -0,0 +1,32 @@
+dir=$1
+
+plots=`ls $dir/*.png`
+
+out=$dir/index.html
+rm $out
+
+echo "<div class=\"ext-box\">" >> $out
+echo "<TABLE border= \"1\">" >> $out
+
+i=0
+for p in $plots;
+do
+
+    name=${p##*/}
+    echo $name
+    echo $i
+
+    if [ $(($i%2)) = 0 ]; then
+        if [ $i -gt 0 ]; then
+            echo "</TR>" >> $out
+        fi
+        echo "<TR>" >> $out
+    fi
+
+    echo "<TD><CENTER>" >> $out
+    echo "<img src=\"$name\" height=\"250\" width=\"500\">" >> $out
+#    echo "<img src=\"$name\" height=\"450\" width=\"600\">" >> $out   
+    echo "</CENTER></TD>" >> $out
+
+    i=$(($i+1))
+done
